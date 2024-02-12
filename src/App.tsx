9b73c316e375f72c03575b9190fead6d81e222c0
Raymond Price <darthbob88@users.noreply.github.com>
2024-02-12 11:01:05 -0800

    Moving the image source into the SingleImage component.

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,13 @@ import './App.css';
 import SingleImage from './SingleImage/SingleImage';
 
 function App() {
-  const [imgSrc, setImgSrc] = useState<string>('https://upload.wikimedia.org/wikipedia/commons/1/1e/Fritz_Haber.png');
   const [pixelSize, setPixelSize] = useState<number>(10);
   return (
     <div className="App">
       <label> Pixel size:     <input type='number' value={pixelSize} onChange={(evt) => setPixelSize(evt.target.valueAsNumber)} />
       </label>
       <br />
-      <label>Image source:   <input type='text' value={imgSrc} onChange={(evt) => setImgSrc(evt.currentTarget.value)} /></label>
-      <br />
-      <SingleImage imgSrc={imgSrc} pixelSize={pixelSize} />
+      <SingleImage pixelSize={pixelSize} />
     </div>
   );
 }
--- src/SingleImage/SingleImage.tsx
+++ src/SingleImage/SingleImage.tsx
@@ -3,16 +3,19 @@ import React, { useState } from 'react';
 import { ImagePixelated } from 'react-pixelate';
 
 type SingleImageProps = {
-    imgSrc: string;
-    pixelSize: number
+    pixelSize: number;
 };
 
-function SingleImage({ imgSrc, pixelSize }: SingleImageProps) {
+function SingleImage({ pixelSize }: SingleImageProps) {
     const [errorText, setErrorText] = useState<string>("");
+    const [imgSrc, setImgSrc] = useState<string>('https://upload.wikimedia.org/wikipedia/commons/1/1e/Fritz_Haber.png');
 
     return (
         <div style={{ border: "1px solid red", width: "60vw", margin: "0 auto" }}>
+            <label>Image source:   <input type='text' value={imgSrc} onChange={(evt) => setImgSrc(evt.currentTarget.value)} /></label>
             <p>{errorText}</p>
+            <br />
+
             <img src={imgSrc} onError={() => setErrorText(`Unable to load from ${imgSrc}`)} />
             <ImagePixelated src={imgSrc} pixelSize={pixelSize} />
         </div>
